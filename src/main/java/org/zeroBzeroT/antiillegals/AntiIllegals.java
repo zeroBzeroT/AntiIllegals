2c8b4d2fc2e30b7871187a24c5087c1bbb964421
Synio <synio@hotmail.fr>
2022-11-11 15:09:50 +0100

    feat(unbreakables) added unbreakables to illegals

--- src/main/java/org/zeroBzeroT/antiillegals/AntiIllegals.java
+++ src/main/java/org/zeroBzeroT/antiillegals/AntiIllegals.java
@@ -33,7 +33,7 @@ public class AntiIllegals extends JavaPlugin {
 
         getConfig().addDefault("bStats", true);
         getConfig().addDefault("nameColors", false);
-        getConfig().addDefault("unbreakables", false);
+        getConfig().addDefault("unbreakables", true);
         getConfig().addDefault("illegalBlocks", true);
         getConfig().addDefault("nbtFurnaces", true);
         getConfig().addDefault("overstackedItems", true);
@@ -190,10 +190,12 @@ public class AntiIllegals extends JavaPlugin {
         }
 
         // Unbreakable
-        if (AntiIllegals.instance.getConfig().getBoolean("unbreakables", false) && itemStack.getType().isItem() && !itemStack.getType().isEdible() && !itemStack.getType().isBlock() && (itemStack.getDurability() > itemStack.getType().getMaxDurability() || itemStack.getDurability() < 0 || itemStack.getItemMeta().isUnbreakable())) {
+        if (AntiIllegals.instance.getConfig().getBoolean("unbreakables", true) && itemStack.getType().isItem() && !itemStack.getType().isEdible() && !itemStack.getType().isBlock() && (itemStack.getDurability() > itemStack.getType().getMaxDurability() || itemStack.getDurability() < 0 || itemStack.getItemMeta().isUnbreakable())) {
             itemStack.setDurability((short) 0);
             itemStack.getItemMeta().setUnbreakable(false);
+            log("Unbreakables", "Removed Unbreakable " + itemStack);
             itemStack.setAmount(0);
+            return ItemState.illegal;
         }
 
         // Illegal blocks
