d2cad987f5bcadc0b067d3de50919a5d38d34a96
crxyne <craynemail@gmail.com>
2024-05-15 20:52:05 +0200

    update version => 1.0.62: optimization for consistent caching of shulkerboxes, preventing random permutations of nbt tags from affecting hashcode

--- src/main/java/org/zeroBzeroT/antiillegals/result/CachedState.java
+++ src/main/java/org/zeroBzeroT/antiillegals/result/CachedState.java
@@ -1,5 +1,6 @@
 package org.zeroBzeroT.antiillegals.result;
 
+import com.google.gson.Gson;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.jetbrains.annotations.NotNull;
@@ -9,16 +10,36 @@ import java.util.Objects;
 
 public record CachedState(@NotNull ItemStack revertedStack, @NotNull ItemState revertedState) {
 
+    /**
+     * hashes the item identity, not the object reference itself
+     * @param itemStack the itemstack to find the hashcode of
+     * @return the hashcode
+     */
     public static int itemStackHashCode(@NotNull final ItemStack itemStack) {
-        final ItemMeta meta = itemStack.getItemMeta();
-
-        // hash the item identity, not the object reference itself
         return Objects.hash(
                 itemStack.getType().ordinal(),
                 itemStack.getDurability(),
                 itemStack.getAmount(),
-                String.valueOf(meta)
+                nbtHashCode(itemStack)
         );
+    }
+
+    @NotNull
+    private static final Gson NBT_GSON = new Gson();
+
+    /**
+     * order-independent nbt hashcode implementation
+     * @param itemStack the itemstack of which the nbt json will be used
+     * @return the hashcode of the nbt, ignoring the order of keys/values to ensure proper identity
+     */
+    public static int nbtHashCode(@NotNull final ItemStack itemStack) {
+        final ItemMeta nbt = itemStack.clone().getItemMeta();
+        if (nbt == null) return 0;
+
+        final String name = nbt.getDisplayName();
+        nbt.setDisplayName(null);
+
+        return Objects.hash(NBT_GSON.toJsonTree(nbt), name);
     }
 
     public void applyRevertedState(@NotNull final ItemStack cached) {
